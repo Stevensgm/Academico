0ff287a58b3a3e81ce2e78a35afe5fb58da75ba9
carlos <stevensgm1@gmail.com>
2025-08-26 00:13:52 -0500

    'se actualizo Estudiante'

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,3 +1,24 @@
 public class Estudiante {
+
+    private String nombre;
+    private int semestreActual, idEstudiante;
+
+
+    public Estudiante ()  {}
+
+    public Estudiante (String nombre, int semestreActual, int idEstudiante){
+     this.nombre = nombre;
+     this.semestreActual = semestreActual;
+     this.idEstudiante = idEstudiante;
+
+    }
+
+
+    public void mostrarInformacion () {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Semestre Actual: " + semestreActual);
+        System.out.println("ID del Estudiante: " + idEstudiante);
+    }
     
 }
+
